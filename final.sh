482f97a1ed4ede6bef649491f5fe9999f5322c41
Inok <inok.dr189@gmail.com>
2021-11-13 11:32:04 +0700

    Initial Commit
    Signed-off-by: I-n-o-k <inok.dr189@gmail.com>

---
+++ final.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd /tmp/rom
+
+. build/envsetup.sh
+export CCACHE_DIR=/tmp/ccache
+export CCACHE_EXEC=$(which ccache)
+export USE_CCACHE=1
+ccache -M 20G
+ccache -o compression=true
+ccache -z
+
+$lunch
+$make -j8
