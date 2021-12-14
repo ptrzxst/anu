4563a2fa3f5558dc93746192598631bd0396d45b
Inok <75870644+I-n-o-k@users.noreply.github.com>
2021-12-14 17:15:01 +0700

    Update kang.sh

--- kang.sh
+++ kang.sh
@@ -21,7 +21,6 @@ curl -L -o /tmp/los/vendor/xiaomi/juice/proprietary/vendor/bin/hw/android.hardwa
 curl -L -o /tmp/los/vendor/xiaomi/juice/proprietary/vendor/etc/init/android.hardware.bluetooth@1.0-service-qti.rc https://gitlab.com/the-muppets/proprietary_vendor_xiaomi/-/raw/lineage-18.1/sm8250-common/proprietary/vendor/etc/init/android.hardware.bluetooth@1.0-service-qti.rc
 curl -L -o /tmp/los/vendor/xiaomi/juice/proprietary/vendor/lib64/libbtnv.so https://gitlab.com/the-muppets/proprietary_vendor_xiaomi/-/raw/lineage-18.1/sm8250-common/proprietary/vendor/lib64/libbtnv.so
 curl -L -o /tmp/los/vendor/xiaomi/juice/proprietary/vendor/lib64/hw/android.hardware.bluetooth@1.0-impl-qti.so https://gitlab.com/the-muppets/proprietary_vendor_xiaomi/-/raw/lineage-18.1/sm8250-common/proprietary/vendor/lib64/hw/android.hardware.bluetooth@1.0-impl-qti.so
-curl -L -o /tmp/los/vendor/xiaomi/juice/proprietary/vendor/lib64/libbtnv.so https://gitlab.com/the-muppets/proprietary_vendor_xiaomi/-/raw/lineage-18.1/sm8250-common/proprietary/vendor/lib64/libbtnv.so
 
 # Kang libaptX from SM8250
 curl -L -o /tmp/los/vendor/xiaomi/juice/proprietary/system_ext/lib64/libaptXHD_encoder.so https://gitlab.com/the-muppets/proprietary_vendor_xiaomi/-/raw/lineage-18.1/sm8250-common/proprietary/system_ext/lib64/libaptXHD_encoder.so
