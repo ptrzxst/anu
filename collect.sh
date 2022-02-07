a77acabba96bee93137c60ac4ad0819e5f6c050a
Inok <75870644+I-n-o-k@users.noreply.github.com>
2022-02-08 05:31:06 +0700

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -5,11 +5,11 @@ cd ~/rom
 export CCACHE_DIR=~/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-ccache -M 20G
+ccache -M 100G -F 0
 ccache -o compression=true
 ccache -z
 $lunch
-make android.hardware.bluetooth@1.0.vendor && make android.hardware.bluetooth@1.1.vendor && $make -j8 &
+$make -j8 &
 sleep 100m
 kill %1
 ccache -s
